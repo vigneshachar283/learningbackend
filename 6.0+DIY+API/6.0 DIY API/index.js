301eee72420eb26565559bc2a5c09202d9ce19cd
Vignesh <vignesh24cs@rnsit.ac.in>
2026-06-18 10:03:14 +0530

    hello

--- 6.0+DIY+API/6.0 DIY API/index.js
+++ 6.0+DIY+API/6.0 DIY API/index.js
@@ -9,6 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
 
+app.get("/random",(req,res)=>
+{
+  const randomIndex=Math.floor(Math.random()*jokes.length);
+  const randomJoke=jokes[randomIndex];
+  res.json(randomJoke);
+})
+
 //2. GET a specific joke
 
 //3. GET a jokes by filtering on the joke type
