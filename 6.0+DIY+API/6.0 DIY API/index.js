b518e380c618f21ae9e99a903ffbf7d800a4e048
Vignesh <vignesh24cs@rnsit.ac.in>
2026-06-18 12:12:13 +0530

    h

--- 6.0+DIY+API/6.0 DIY API/index.js
+++ 6.0+DIY+API/6.0 DIY API/index.js
@@ -32,6 +32,17 @@ app.get("/filter",(req,res)=>{
 })
 
 //4. POST a new joke
+app.post("/jokes",(req,res)=>{
+  const newJoke={
+    id:jokes.length+1,
+    jokeText:req.body.text,
+    jokeType:req.body.type
+
+  }
+
+  jokes.push(newJoke);
+  res.json(newJoke);
+})
 
 //5. PUT a joke
 
